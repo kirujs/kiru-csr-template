7d8f07e4909c6ac82a0233196b5b8e5b4d660bd8
LankyMoose <rausten93@gmail.com>
2026-03-11 23:39:46 +1300

    ^ deps, simplify tailwind setup, tidy up counter example

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite"
+import tailwindcss from "@tailwindcss/vite"
 import kiru from "vite-plugin-kiru"
 
 export default defineConfig({
-  plugins: [kiru({ devtools: true })],
+  plugins: [tailwindcss(), kiru()],
 })
